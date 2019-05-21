f4bf342d3b71628af6bf2701e94b20c1159704e0
DB <davidrberry88@hotmail.com>
2019-05-21 20:07:04 +0100

    Added Positions to the index.cs

--- FootballManagement.WebApp/Controllers/PlayerController.cs
+++ FootballManagement.WebApp/Controllers/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Web.Mvc;
 using FootballManagement.WebApp.Models;
 using System.Configuration;
+using System.Collections.Generic;
 
 namespace FootballManagement.WebApp.Controllers
 {
     public class PlayerController : Controller
-    {
+    {                
+
         public ActionResult Index()
         {
-            return View();
+
+            //This is an example of how I would build a list of players from an API call / Database and send it to the view
+            List<PlayerViewModel> playlist = new List<PlayerViewModel>();
+            playlist.Add(new PlayerViewModel() { Id = 1, FirstName = "Test", LastName = "TestLastName", NickName = "TiTi", Positions = 0, ShirtNumber = 14, Team = 1 });
+
+            return View(playlist);
         }
     }
 }
